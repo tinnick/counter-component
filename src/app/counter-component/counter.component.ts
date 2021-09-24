d64a80b61b449da7970ef49dac9365f0e0d8311b
tinnick <tinnick@gmail.com>
2021-09-24 23:04:30 +0900

    2 files modified

--- src/app/counter-component/counter.component.ts
+++ src/app/counter-component/counter.component.ts
@@ -4,10 +4,9 @@ import {
   keyframes,
   style,
   AnimationPlayer,
-  query
+  query,
 } from '@angular/animations';
-import { Component, Input, ElementRef } from '@angular/core';
-
+import { Component, Input, ElementRef, OnInit } from '@angular/core';
 
 declare interface ISequenceClasses {
   digits: string[];
@@ -27,12 +26,12 @@ declare interface ISequenceNumber {
 @Component({
   selector: 'app-counter',
   templateUrl: './counter.component.html',
-  styleUrls: ['./counter.component.scss']
+  styleUrls: ['./counter.component.scss'],
 })
-export class CounterComponent {
+export class CounterComponent implements OnInit {
   @Input() animationSeconds: number = 1;
-  @Input() from: number = 0;
-  @Input() to: number = 60;
+  @Input() from: number = 0.0384;
+  @Input() to: number = 60.123;
   @Input() decimalPlaces: number = 2;
   @Input() isTimeCounter: boolean = true;
 
@@ -56,10 +55,16 @@ export class CounterComponent {
     return this.elementRef && this.elementRef.nativeElement;
   }
 
-  constructor(private animationBuilder: AnimationBuilder, private elementRef: ElementRef) {}
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private elementRef: ElementRef
+  ) {}
 
-  public startCounter(element: HTMLDivElement): void {
-    this.animateElement = element;
+  public ngOnInit(): void {
+    this.startCounter();
+  }
+
+  public startCounter(): void {
     this.digits = this.getDigits();
     this.decimals = this.getDecimals();
 
@@ -69,28 +74,33 @@ export class CounterComponent {
     const [diffDigits, diffDecimals] = String(this.to - this.from)
       .slice(0, this.digits.length + this.decimalPlaces + 1)
       .split('.')
-      .map(str => str.split('').map((_, index, array) => index === 0 ? '0' : array.slice(0, index).join('')));
+      .map((str) =>
+        str
+          .split('')
+          .map((_, index, array) =>
+            index === 0 ? '0' : array.slice(0, index).join('')
+          )
+      );
 
     const sequences = [];
 
     diffDigits.forEach((numString, index) => {
-      const klass = this.sequenceClasses[index];
+      const klass = this.sequenceClasses.digits[index];
       const num = +numString;
       const anims = [];
 
       if (num) {
-
         const fromSequence = this.sequenceNumbers.from.digits[index];
         const toSequence = this.sequenceNumbers.to.digits[index];
 
-        const fromToLoopOffset = (1 - (fromSequence / 10)) / num;
-        const loopToToOffset = (1 - (toSequence / 10)) / num;
+        const fromToLoopOffset = (1 - fromSequence / 10) / num;
+        const loopToToOffset = (1 - toSequence / 10) / num;
 
         const animSegment = (1 - fromToLoopOffset - loopToToOffset) / num;
 
         anims.push([
           style({ transform: `translateY(-${fromSequence}em)`, offset: 0 }),
-          style({ transform: 'translateY(-10em)', offset: fromToLoopOffset })
+          style({ transform: 'translateY(-10em)', offset: fromToLoopOffset }),
         ]);
 
         for (let i = 0; i <= num; i++) {
@@ -99,40 +109,47 @@ export class CounterComponent {
 
           anims.push([
             style({ transform: `translateY(0em)`, offset: startOffset }),
-            style({ transform: 'translateY(-10em)', offset: endOffset })
-          ])
+            style({ transform: 'translateY(-10em)', offset: endOffset }),
+          ]);
         }
 
         anims.push([
           style({ transform: 'translateY(0em)', offset: 1 - loopToToOffset }),
-          style({ transform: `translateY(-${toSequence}em)`, offset: 1 })
+          style({ transform: `translateY(-${toSequence}em)`, offset: 1 }),
         ]);
-
       }
 
-      sequences.push(query(klass, animate(`${this.animationSeconds}s ease-in-out`, keyframes([ ...anims ])), { optional: true }));
+      sequences.push(
+        query(
+          klass,
+          animate(
+            `${this.animationSeconds}s ease-in-out`,
+            keyframes([...anims])
+          ),
+          { optional: true }
+        )
+      );
     });
 
     const diffDecimalsBase = diffDigits.join('');
 
     diffDecimals.forEach((numString, index) => {
-      const klass = this.sequenceClasses[diffDigits.length - 1 + index];
+      const klass = this.sequenceClasses.decimals[index];
       const num = +(diffDecimalsBase + numString);
       const anims = [];
 
       if (num) {
-
         const fromSequence = this.sequenceNumbers.from.decimals[index];
         const toSequence = this.sequenceNumbers.to.decimals[index];
 
-        const fromToLoopOffset = (1 - (fromSequence / 10)) / num;
-        const loopToToOffset = (1 - (toSequence / 10)) / num;
+        const fromToLoopOffset = (1 - fromSequence / 10) / num;
+        const loopToToOffset = (1 - toSequence / 10) / num;
 
         const animSegment = (1 - fromToLoopOffset - loopToToOffset) / num;
 
         anims.push([
           style({ transform: `translateY(-${fromSequence}em)`, offset: 0 }),
-          style({ transform: 'translateY(-10em)', offset: fromToLoopOffset })
+          style({ transform: 'translateY(-10em)', offset: fromToLoopOffset }),
         ]);
 
         for (let i = 0; i <= num; i++) {
@@ -141,21 +158,29 @@ export class CounterComponent {
 
           anims.push([
             style({ transform: `translateY(0em)`, offset: startOffset }),
-            style({ transform: 'translateY(-10em)', offset: endOffset })
-          ])
+            style({ transform: 'translateY(-10em)', offset: endOffset }),
+          ]);
         }
 
         anims.push([
           style({ transform: 'translateY(0em)', offset: 1 - loopToToOffset }),
-          style({ transform: `translateY(-${toSequence}em)`, offset: 1 })
+          style({ transform: `translateY(-${toSequence}em)`, offset: 1 }),
         ]);
-
       }
 
-      sequences.push(query(klass, animate(`${this.animationSeconds}s ease-in-out`, keyframes([ ...anims ])), { optional: true }));
+      sequences.push(
+        query(
+          klass,
+          animate(
+            `${this.animationSeconds}s ease-in-out`,
+            keyframes([...anims])
+          ),
+          { optional: true }
+        )
+      );
     });
 
-    this.animationBuilder.build(...sequences).create(this.element).play();
+    this.animationBuilder.build(sequences).create(this.element).play();
   }
 
   private getDigits(): number[] {
@@ -172,20 +197,33 @@ export class CounterComponent {
 
     return {
       from: {
-        digits: this.digits.slice(0, this.digits.length - fromDigits.length).concat([...fromDigits].map(Number)),
-        decimals: this.decimals.slice(0, this.decimals.length - fromDecimals.length).concat([...fromDecimals].map(Number))
+        digits: this.digits
+          .slice(0, this.digits.length - fromDigits.length)
+          .concat([...fromDigits].map(Number)),
+        decimals: this.decimals
+          .slice(0, this.decimals.length - fromDecimals.length)
+          .concat([...fromDecimals].map(Number)),
       },
       to: {
-        digits: this.digits.slice(0, this.digits.length - toDigits.length).concat([...toDigits].map(Number)),
-        decimals: this.decimals.slice(0, this.decimals.length - toDecimals.length).concat([...toDecimals].map(Number))
-      }
-    }
+        digits: this.digits
+          .slice(0, this.digits.length - toDigits.length)
+          .concat([...toDigits].map(Number)),
+        decimals: this.decimals
+          .slice(0, this.decimals.length - toDecimals.length)
+          .concat([...toDecimals].map(Number)),
+      },
+    };
   }
 
   private buildSequenceClasses(): ISequenceClasses {
     return {
-      digits: this.sequenceNumbers.to.digits.map((_, index, { length }) => `digit-${new Array(length - index).fill(0).join('')}`),
-      decimals: this.sequenceNumbers.to.decimals.map((_, index) => `decimal-${new Array(index + 1).fill(0)}`)
-    }
+      digits: this.sequenceNumbers.to.digits.map(
+        (_, index, { length }) =>
+          `digit-${new Array(length - index).fill(0).join('')}`
+      ),
+      decimals: this.sequenceNumbers.to.decimals.map(
+        (_, index) => `decimal-${new Array(index + 1).fill(0)}`
+      ),
+    };
   }
 }
